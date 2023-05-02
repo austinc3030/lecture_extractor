45f980a9cd5b4a672d476cb592241a047fd4a085
Conn <M64200@NGC.AUTHORITY>
2023-05-02 09:23:07 -0400

    Update lecture_extractor.py

--- lecture_extractor.py
+++ lecture_extractor.py
@@ -1,6 +1,12 @@
 #!/bin/python3
 
+import math, operator
+from PIL import Image
+import sys
+import os
+import glob
 import subprocess
+import shutil
 
 # Filename
 input_filename = "sample.wmv"
@@ -8,10 +14,35 @@ input_filename = "sample.wmv"
 # Number of frames per second to extract from video. Note: A high sample rate here likely isn't needed for a slide show
 sampling_rate = 1
 
+
+def compare(file1, file2):
+    image1 = Image.open(file1)
+    image2 = Image.open(file2)
+    h1 = image1.histogram()
+    h2 = image2.histogram()
+    rms = math.sqrt(reduce(operator.add,
+                           map(lambda a,b: (a-b)**2, h1, h2))/len(h1))
+    return rms
+
+
 if __name__ == '__main__':
     ffmpeg_cmd = ["ffmpeg",
-                  "-v", "quiet", "-stats"
+                  "-v", "quiet", "-stats",
                   "-i", input_filename,
                   "-vf", "fps={sampling_rate}".format(sampling_rate=sampling_rate),
-                  "{input_filename}_%010d.png".format(input_filename=input_filename)]
+                  "extraction/{input_filename}_%010d.png".format(input_filename=input_filename)]
     subprocess.call(ffmpeg_cmd)
+
+    filelist = glob.glob(os.path.join("extraction", '*.png'))
+    filelist.sort()
+    for ii in range(0, len(filelist)):
+        if ii < len(filelist)-1:
+            if compare(filelist[ii], filelist[ii+1]) == 0:
+                print 'Found similar images: ' + filelist[ii] + " and " + filelist[ii+1]
+            else:
+                print 'Found unique image: ' + filelist[ii]
+                head, tail = os.path.split(filelist[ii])
+                shutil.copyfile(filelist[ii], sys.argv[2] + os.path.sep + tail)
+        else:
+            shutil.copyfile(filelist[ii], sys.argv[2] + os.path.sep + tail)
+    shutil.rmtree("extraction")
