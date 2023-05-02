4260788c26ed337fc44f78b988acd8bc36bd828c
Conn <M64200@NGC.AUTHORITY>
2023-05-02 11:34:31 -0400

    Update lecture_extractor.py

--- lecture_extractor.py
+++ lecture_extractor.py
@@ -1,3 +1,4 @@
+austinc3030@lecture-extractor:~/lecture_extractor$ cat lecture_extractor.py 
 #!/bin/python3
 
 import math, operator
@@ -23,10 +24,10 @@ def compare(image1=None, image2=None, similarity=80):
     hash_size = 8
     diff_limit = int(threshold*(hash_size**2))
 
-    hash1 = imagehash.average_hash(image1, hash_size).hash
-    hash2 = imagehash.average_hash(image2, hash_size).hash
+    hash1 = imagehash.average_hash(image1)
+    hash2 = imagehash.average_hash(image2)
 
-    difference = hash1 != hash2
+    difference = hash1 - hash2
 
     return difference
 
@@ -40,14 +41,13 @@ def deduplicate(location, similarity=80):
             image1 = Image.open(filelist[ii])
             image2 = Image.open(filelist[ii+1])
 
-            print("Difference: {}".format(compare(image1=image1, image2=image2, similarity=80)))
-
-            # if compare(image1, image2) != 0:
-            #     #head, tail = os.path.split(filelist[ii])
-            #     #shutil.copyfile(filelist[ii], location + os.path.sep + tail)
-            #     count += 1
+            difference = compare(image1, image2)
+            if difference != 0:
+                head, tail = os.path.split(filelist[ii])
+                shutil.copyfile(filelist[ii], location + os.path.sep + tail)
+                count += 1
         else:
-            #shutil.copyfile(filelist[ii], location + os.path.sep + tail)
+            shutil.copyfile(filelist[ii], location + os.path.sep + tail)
             count += 1
     #shutil.rmtree("extraction")
 
@@ -55,26 +55,17 @@ def deduplicate(location, similarity=80):
 
 
 if __name__ == '__main__':
+
     if not os.path.exists("extraction"):
         os.mkdir("extraction")
-    else:
-        sys.exit("extraction already exists, exiting.")
-    
+        ffmpeg_cmd = ["ffmpeg",
+                    "-v", "quiet", "-stats",
+                    "-i", input_filename,
+                    "-vf", "fps={sampling_rate}".format(sampling_rate=sampling_rate),
+                    "extraction/{input_filename}_%010d.png".format(input_filename=input_filename)]
+        subprocess.call(ffmpeg_cmd)
+        
     if not os.path.exists("deduplicated"):
         os.mkdir("deduplicated")
-    else:
-        sys.exit("deduplicated already exists, exiting.")
-
-    ffmpeg_cmd = ["ffmpeg",
-                  "-v", "quiet", "-stats",
-                  "-i", input_filename,
-                  "-vf", "fps={sampling_rate}".format(sampling_rate=sampling_rate),
-                  "extraction/{input_filename}_%010d.png".format(input_filename=input_filename)]
-    subprocess.call(ffmpeg_cmd)
-    location = "deduplicated"
-
-    count = deduplicate(location=location, similarity=80)
-
-    # for i in range(1, 99):
-    #     count = deduplicate(location=location, similarity=i)
-    #     print("With similarity={similarity}, found {count} slides.".format(similarity=i, count=count))
+        location = "deduplicated"
+        print(deduplicate(location=location, similarity=80))
