183e0ad8d54a6accc1cc50850106bf334d792754
Conn <M64200@NGC.AUTHORITY>
2023-05-02 16:28:14 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -4,5 +4,8 @@ import ffmpeg
 
 filename="sample.wmv"
 probe = ffmpeg.probe(filename)
-time = float(probe['streams'][0]['duration'])
-print(time)
+duration_exact = float(probe['streams'][0]['duration'])
+duration_whole_second = int(duration_exact)
+
+for i in range(0, duration_whole_second):
+    ffmpeg.input(filename, ss=i).output('filename_' + str(i) + '.png', vframes=1).run()
