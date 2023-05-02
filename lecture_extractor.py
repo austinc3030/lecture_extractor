e9671c6f59cbfefe67aa82440cd7c1e6695ee8f5
Conn <M64200@NGC.AUTHORITY>
2023-05-02 10:42:14 -0400

    Update lecture_extractor.py

--- lecture_extractor.py
+++ lecture_extractor.py
@@ -8,6 +8,8 @@ import glob
 import subprocess
 import shutil
 import functools
+import imagehash
+import numpy as np
 
 # Filename
 input_filename = "sample.wmv"
@@ -15,15 +17,52 @@ input_filename = "sample.wmv"
 # Number of frames per second to extract from video. Note: A high sample rate here likely isn't needed for a slide show
 sampling_rate = 1
 
-
-def compare(file1, file2):
-    image1 = Image.open(file1)
-    image2 = Image.open(file2)
+def compare_1(image1=None, image2=None):
     h1 = image1.histogram()
     h2 = image2.histogram()
     rms = math.sqrt(functools.reduce(operator.add,
                                      map(lambda a,b: (a-b)**2, h1, h2))/len(h1))
     return rms
+
+
+def compare_2(image1=None, image2=None, similarity=80):
+    threshold = 1 - similarity/100
+    hash_size = 8
+    diff_limit = int(threshold*(hash_size**2))
+
+    hash1 = imagehash.average_hash(image1, hash_size).hash
+    hash2 = imagehash.average_hash(image2, hash_size).hash
+
+    if np.count_nonzero(hash1 != hash2) <= diff_limit:
+        return 0
+    else:
+        return 1
+
+
+def deduplicate(location, similarity=80, method=1):
+    filelist = glob.glob(os.path.join("extraction", '*.png'))
+    filelist.sort()
+    for ii in range(0, len(filelist)):
+        if ii < len(filelist)-1:
+            image1 = Image.open(filelist[ii])
+            image2 = Image.open(filelist[ii+1])
+
+            if method == 1:
+                unique = compare_1(image1, image2)
+            elif method == 2:
+                unique = compare_2(image1, image2)
+            else:
+                unique = compare_1(image1, image2)
+
+            if unique == 0:
+                print("Found similar images: {file1} and {file2}".format(file1=filelist[ii], file2=filelist[ii+1]))
+            else:
+                print("Found unique image: {file}".format(file=filelist[ii]))
+                head, tail = os.path.split(filelist[ii])
+                shutil.copyfile(filelist[ii], location + os.path.sep + tail)
+        else:
+            shutil.copyfile(filelist[ii], location + os.path.sep + tail)
+    shutil.rmtree("extraction")
 
 
 if __name__ == '__main__':
@@ -44,16 +83,5 @@ if __name__ == '__main__':
                   "extraction/{input_filename}_%010d.png".format(input_filename=input_filename)]
     subprocess.call(ffmpeg_cmd)
 
-    filelist = glob.glob(os.path.join("extraction", '*.png'))
-    filelist.sort()
-    for ii in range(0, len(filelist)):
-        if ii < len(filelist)-1:
-            if compare(filelist[ii], filelist[ii+1]) == 0:
-                print("Found similar images: {file1} and {file2}".format(file1=filelist[ii], file2=filelist[ii+1]))
-            else:
-                print("Found unique image: {file}".format(file=filelist[ii]))
-                head, tail = os.path.split(filelist[ii])
-                shutil.copyfile(filelist[ii], "deduplicated" + os.path.sep + tail)
-        else:
-            shutil.copyfile(filelist[ii], "deduplicated" + os.path.sep + tail)
-    shutil.rmtree("extraction")
+    location = "deduplicated"
+    deduplicate(location, similarity=80, 1)
