ee8be6f112cd151c9306f63c19539bc4d6cc004d
Conn <M64200@NGC.AUTHORITY>
2023-05-02 08:59:50 -0400

    Update lecture_extractor.py

--- lecture_extractor.py
+++ lecture_extractor.py
@@ -1,20 +1,16 @@
 #!/bin/python3
 
-import ffmpeg
 import subprocess
 
 # Filename
 input_filename = "sample.wmv"
 
 # Number of frames per second to extract from video. Note: A high sample rate here likely isn't needed for a slide show
-video_sampling_rate = 1
+sampling_rate = 1
 
 if __name__ == '__main__':
-    cmd = ["ffmpeg", "-i", input_filename, "-vf", "fps=1", "$filename%010d.png"]
-    subprocess.call(cmd)
-    
-    
-    #stream = ffmpeg.input(input_filename)
-    #stream = ffmpeg.filter(stream, 'fps', fps=1)
-    #stream = ffmpeg.filter_multi_output(stream, "{input_filename}_%010d.png".format(input_filename=input_filename))
-    #ffmpeg.run(steam)
+    ffmpeg_cmd = ["ffmpeg",
+                  "-i", input_filename,
+                  "-vf", "fps={sampling_rate}".format(sampling_rate=sampling_rate),
+                  "{input_filename}%010d.png".format(input_filename=input_filename)]
+    subprocess.call(ffmpeg_cmd)
